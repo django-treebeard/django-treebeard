fe22019ab88423d0ae732684715b1ed01c9224b6
Gustavo Picon <tabo@tabo.pe>
2008-09-21 20:20:05 +0000

    more docstring fun
    --HG--
    extra : convert_revision : svn%3A6f0eb378-856a-11dd-b774-4f160e1b508b/trunk%4020

--- treebeard/__init__.py
+++ treebeard/__init__.py
@@ -17,6 +17,57 @@
        `treebeard-tests
        <http://code.google.com/p/django-treebeard/source/browse/trunk/treebeard/tests.py>`_
 
+    ``django-treebeard`` is an efficient implementation of Materialized Path
+    trees for Django 1.0+, as described by `Vadim Tropashko`_ in `SQL Design
+    Patterns`_. Materialized Path is probably the fastest way of working with
+    trees in SQL without the need of extra work in the database, like Oracle's
+    ``CONNECT BY`` or sprocs and triggers for nested intervals.
+
+    In a materialized path approach, every node in the tree will have a
+    :attr:`~treebeard.MPNode.path` attribute, where the full path from the root
+    to the node will be stored. This has the advantage of needing very simple
+    and fast queries, at the risk of inconsistency because of the
+    denormalization of ``parent``/``child`` foreign keys. This can be prevented
+    with transactions (and of course you are already using them, right?).
+
+    ``django-treebeard`` stores two extra fields in every node:
+    :attr:`~treebeard.MPNode.depth` and :attr:`~treebeard.MPNode.numchild`.
+    This makes the read operations faster, at the cost of a little more
+    maintenance on tree updates/inserts/deletes. Don't worry, even with these
+    extra steps, materialized path is more efficient than other approaches.
+
+    .. note::
+       
+       The materialized path approach makes heavy use of ``LIKE`` in your
+       database, with clauses like ``WHERE path LIKE '002003%'``. If you think
+       that ``LIKE`` is too slow, you're right, but in this case the
+       :attr:`~treebeard.MPNode.path` field is indexed in the database, and all
+       ``LIKE`` clauses that don't **start** with a ``%`` character will use the
+       index. This is what makes the materialized path approach so fast.
+
+    ``django-treebeard`` uses `Django Model Inheritance with abstract classes`_
+    to let you define your own models. To use ``django-treebeard``:
+
+       1. Download a release from the `treebeard download page`_ or get a
+          development version from the `djangotree subversion repository`.
+       2. Run :command:`python setup.py install`
+       3. Add ``'treebeard'`` to the ``INSTALLED_APPS`` section in your django
+          settings file.
+       4. Create a new model that inherits from :class:`treebeard.MPNode`
+       5. Run :command:`python manage.py syncdb`
+
+    Read the :class:`treebeard.MPNode` API reference for detailed info.
+
+    .. _`Vadim Tropashko`: http://vadimtropashko.wordpress.com/
+    .. _`Sql Design Patterns`:
+       http://www.rampant-books.com/book_2006_1_sql_coding_styles.htm
+    .. _`Django Model Inheritance with abstract classes`:
+      http://docs.djangoproject.com/en/dev/topics/db/models/#abstract-base-classes
+    .. _`treebeard download page`:
+       http://code.google.com/p/django-treebeard/downloads/list
+    .. _`treebeard subversion repository`:
+       http://code.google.com/p/django-treebeard/source/checkout
+
 """
 
 VERSION = (0, 9, 'svn')
