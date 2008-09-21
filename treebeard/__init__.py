aede74b0375d81d04a7b2ebdfb7403acf824e408
Gustavo Picon <tabo@tabo.pe>
2008-09-21 18:29:54 +0000

    License changed to apache 2, added examples in docstrings

--- treebeard/__init__.py
+++ treebeard/__init__.py
@@ -1,15 +1,25 @@
 # -*- coding: utf-8 -*-
 """
-    treebeard
-    =========
-    
-    Efficient Materialized Path tree implementation for Django 1.0+
 
+    treebeard
+    ---------
+
+    :synopsys: Efficient Materialized Path tree implementation
+               for Django 1.0+
     :copyright: 2008 by Gustavo Picon
-    :license: BSD
+    :license: Apache License 2.0
+    :version: 0.9-svn
+    :url: http://code.google.com/p/django-treebeard/
+    :documentation:
+       `treebeard-docs
+       <http://django-treebeard.googlecode.com/svn/docs/index.html>`_
+    :examples:
+       `treebeard-tests
+       <http://code.google.com/p/django-treebeard/source/browse/trunk/treebeard/tests.py>`_
+
 """
 
-VERSION = (0, 9, 'pre')
+VERSION = (0, 9, 'svn')
 
 from models import MPNode, InvalidPosition, InvalidMoveToDescendant
 
