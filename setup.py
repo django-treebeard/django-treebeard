1144e01f0a2e6e5d82e6b6bf807d67f3a46bbe42
Gustavo Picon <tabo@tabo.pe>
2008-09-23 07:48:43 +0000

    added setup.py (distutils)

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import os
+from distutils.core import setup
+
+numconv = __import__('treebeard')
+version_tuple = numconv.VERSION
+version = "%d.%d" % version_tuple[:2]
+if version_tuple[2] is not None:
+    svnf = os.path.join(os.path.dirname(os.path.abspath(numconv.__file__)),
+                        '.svn/entries')
+    gotdir = False
+    version = '%s_%s' % (version, str(version_tuple[2]))
+    if os.path.isfile(svnf):
+        for ln in open(svnf):
+            ln = ln.rstrip()
+            if gotdir:
+                version = '%s_rev%s' % (version, ln.replace(' ', ''))
+                break
+            if ln == 'dir':
+                gotdir = True
+
+setup(
+    name = 'django-treebeard',
+    version = version,
+    url = 'http://code.google.com/p/django-treebeard/',
+    author = 'Gustavo Picon',
+    author_email = 'gpicon@gmail.com',
+    license = 'Apache License 2.0',
+    packages = ['treebeard'],
+    description = 'Efficient Materialized Path tree implementation for'
+                  ' Django 1.0+',
+)
+
