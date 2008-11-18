f788bfb493a7f1dd908a2f9498a433ae0f4d869b
Gustavo Picon <tabo@tabo.pe>
2008-11-18 00:00:25 +0000

    ready for a 1.0 release...

--- docs/conf.py
+++ docs/conf.py
@@ -42,9 +42,9 @@ copyright = '2008, Gustavo Picon'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '0.9'
+version = '1.0'
 # The full version, including alpha/beta/rc tags.
-release = '0.9'
+release = '1.0'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
