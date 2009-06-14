5360fd72d7556d7d531e6d84f3adb4615788694d
Gustavo Picon <tabo@tabo.pe>
2009-06-14 09:06:17 +0000

    starting work on 1.2
    --HG--
    extra : convert_revision : svn%3A6f0eb378-856a-11dd-b774-4f160e1b508b/trunk%4085

--- docs/conf.py
+++ docs/conf.py
@@ -42,9 +42,9 @@ copyright = '2008, Gustavo Picon'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '1.1'
+version = '1.2'
 # The full version, including alpha/beta/rc tags.
-release = '1.1'
+release = '1.2a'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
--- setup.py
+++ setup.py
@@ -2,32 +2,14 @@
 # -*- coding: utf-8 -*-
 
 
-import os
 from distutils.core import setup
-
-treebeard = __import__('treebeard')
-version_tuple = treebeard.VERSION
-version = "%d.%d" % version_tuple[:2]
-if version_tuple[2] is not None:
-    svnf = os.path.join(os.path.dirname(os.path.abspath(treebeard.__file__)),
-                        '.svn/entries')
-    gotdir = False
-    version = '%s_%s' % (version, str(version_tuple[2]))
-    if os.path.isfile(svnf):
-        for ln in open(svnf):
-            ln = ln.rstrip()
-            if gotdir:
-                version = '%s_rev%s' % (version, ln.replace(' ', ''))
-                break
-            if ln == 'dir':
-                gotdir = True
 
 setup(
     name = 'django-treebeard',
-    version = version,
+    version = '1.2a',
     url = 'http://code.google.com/p/django-treebeard/',
     author = 'Gustavo Picon',
-    author_email = 'gpicon@gmail.com',
+    author_email = 'tabo@gpicon.org',
     license = 'Apache License 2.0',
     packages = ['treebeard'],
     description = 'Efficient Materialized Path tree implementation for'
