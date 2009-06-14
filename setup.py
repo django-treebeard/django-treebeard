744ebf68eb5b7d254555c3566fac0f38589ea513
Gustavo Picon <tabo@tabo.pe>
2009-06-14 21:35:59 +0000

    preparing setup.py for pypi
    --HG--
    extra : convert_revision : svn%3A6f0eb378-856a-11dd-b774-4f160e1b508b/trunk%40100

--- setup.py
+++ setup.py
@@ -1,18 +1,32 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 
 from distutils.core import setup
 
+version = '1.2a'
+
+classifiers = [
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: Apache Software License",
+    "Programming Language :: Python",
+    "Operating System :: OS Independent",
+    "Topic :: Software Development :: Libraries",
+    "Topic :: Utilities",
+    "Environment :: Web Environment",
+    "Framework :: Django",
+]
+
 setup(
-    name = 'django-treebeard',
-    version = '1.2a',
-    url = 'http://code.google.com/p/django-treebeard/',
-    author = 'Gustavo Picon',
-    author_email = 'tabo@gpicon.org',
-    license = 'Apache License 2.0',
-    packages = ['treebeard'],
-    description = 'Efficient Materialized Path tree implementation for'
-                  ' Django 1.0+',
+    name='django-treebeard',
+    version=version,
+    url='http://code.google.com/p/django-treebeard/',
+    author='Gustavo Picon',
+    author_email='tabo@gpicon.org',
+    license='Apache License 2.0',
+    packages=['treebeard'],
+    description='Efficient Materialized Path tree implementation for'
+                ' Django 1.0+',
+    classifiers=classifiers,
+    #long_description=__doc__,
 )
 
