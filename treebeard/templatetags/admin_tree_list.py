fe1f898a2c1049cc2cee73805cf9d6902b7758f4
Larry Chan <larry@18plans.com>
2010-01-15 13:08:51 +0800

    Added support for raw id fields

--- treebeard/templatetags/admin_tree_list.py
+++ treebeard/templatetags/admin_tree_list.py
@@ -10,33 +10,44 @@
     :license: Apache License 2.0
 
     Original contribution by aleh.fl
-
+    
+    Additional hack added to allow raw_id_fields
+    by Larry Chan
 """
 
-from django.template import Library
+from django.template import resolve_variable
+from django import template
+from django.template import Library, Node
+
 
 register = Library()
 
+def __line(node, request):
+    raw_id_fields = None
+    if 't' in request.GET:
+        if request.GET['t'] == 'id':
+            raw_id_fields = """
+            onclick="opener.dismissRelatedLookupPopup(window, '%d'); return false;"
+            """ % (node.id,)
 
-def __line(node):
     return ('<input type="checkbox" class="action-select" value="%d" '
-            'name="_selected_action" /><a href="%d/">%s</a>') % (node.id,
+            'name="_selected_action" /><a href="%d/" %s>%s</a>') % (node.id,
                                                                  node.id,
-                                                                 str(node))
+                                                                 raw_id_fields,
+                                                                 str(node),)
 
-
-def __subtree(node):
+def __subtree(node, request):
     tree = ''
     for subnode in node.get_children():
-        tree = tree + '<li>%s</li>' % __subtree(subnode)
+        tree = tree + '<li>%s</li>' % __subtree(subnode, request)
     if tree:
         tree = '<ul>%s</ul>' % tree
-    return __line(node) + tree
+    return __line(node, request) + tree
 
 
 @register.simple_tag
-def result_tree(cl):
+def result_tree(cl, request):
     tree = ''
     for root_node in cl.model.get_root_nodes():
-        tree = tree + '<li>%s</li>' % __subtree(root_node)
+        tree = tree + '<li>%s</li>' % __subtree(root_node, request)
     return "<ul>%s</ul>" % tree
