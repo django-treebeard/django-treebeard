af31fd0fdb57b9c37a28249a2b85509574632ab3
Jj <jjdelc@gmail.com>
2010-10-19 16:31:24 -0500

    Expansion and collapseeee

--- treebeard/admin.py
+++ treebeard/admin.py
@@ -4,9 +4,21 @@ from django.contrib import admin
 
 from treebeard.forms import MoveNodeForm
 
+from pprint import pprint
 
 class TreeAdmin(admin.ModelAdmin):
     "Django Admin class for treebeard"
     change_list_template = 'admin/tree_change_list.html'
     form = MoveNodeForm
-    ordering = ('path',)
+
+    """
+    def queryset(self, request):
+        print "Get tree"
+        pprint(self.model.get_tree())
+        print "annotated"
+        pprint(self.model.get_annotated_list())
+        print "Bulk"
+        pprint(self.model.dump_bulk())
+        return self.model.objects.all()#.order_by('-path')
+        return self.model.get_tree().reverse()
+    """
