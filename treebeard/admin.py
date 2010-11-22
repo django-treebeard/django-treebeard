b8266cfdf12fc73980b1cd1a819ef288ba928ac9
Jj <jjdelc@gmail.com>
2010-11-22 18:57:00 -0500

    Don't disable drag handlers on p(age) and all GET params

--- treebeard/admin.py
+++ treebeard/admin.py
@@ -10,6 +10,7 @@ from treebeard.templatetags.admin_tree import check_empty_dict
 from treebeard.exceptions import InvalidPosition, MissingNodeOrderBy, InvalidMoveToDescendant, PathOverflow
 
 class TreeChangeList(ChangeList):
+
     def get_ordering(self):
         """
         Overriding default's ChangeList.get_ordering so we don't sort the
@@ -18,7 +19,6 @@ class TreeChangeList(ChangeList):
         if not check_empty_dict(self.params):
             return super(TreeChangeList, self).get_ordering()
         return None, 'asc'
-
 
 
 class TreeAdmin(admin.ModelAdmin):
