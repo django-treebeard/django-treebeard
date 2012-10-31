3401cd8ac7f3c241865c3039b4cc52b0d8735d49
Gustavo Picon <tabo@tabo.pe>
2012-10-31 03:46:11 -0500

    treebeard needs django 1.3+

--- setup.py
+++ setup.py
@@ -34,10 +34,10 @@ setup_args = dict(
     package_dir={'treebeard': 'treebeard'},
     package_data={
         'treebeard': ['templates/admin/*.html', 'static/treebeard/*']},
-    description='Efficient tree implementations for Django 1.2+',
+    description='Efficient tree implementations for Django 1.3+',
     long_description=open(root_dir() + '/README').read(),
     cmdclass={'test': pytest_test},
-    install_requires=['Django>=1.2'],
+    install_requires=['Django>=1.3'],
     tests_require=['pytest'],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
