b9e5fd43d9d9223c2d28122f6f112ea747f1baf6
Gustavo Picon <tabo@tabo.pe>
2012-11-05 01:43:12 -0500

    Improved tox settings. Put jenkins scripts in version control

---
+++ treebeard/tests/jenkins/rm_workspace_coverage.py
@@ -0,0 +1,21 @@
+"""Remove `.coverage.$HOST.$ID` files from previous runs.
+
+In Python because of portability with Windows.
+"""
+
+import os
+import sys
+
+
+def main():
+    workspace = os.environ['WORKSPACE']
+    for filename in os.listdir(workspace):
+        if filename.startswith('.coverage.'):
+            file_full_name = os.path.join(workspace, filename)
+            sys.stdout.write(
+                '* Removing old .coverage file: `%s`\n' % file_full_name)
+            os.unlink(file_full_name)
+    sys.stdout.flush()
+
+if __name__ == '__main__':
+    main()
