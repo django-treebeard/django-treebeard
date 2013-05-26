60eb4e2ff0110a584941fdb317d860d9a7e0a5f3
Gustavo Picon <tabo@tabo.pe>
2013-05-26 18:16:08 -0500

    Using the tests settings when building documentation.

--- docs/conf.py
+++ docs/conf.py
@@ -9,8 +9,10 @@ Reference: http://sphinx.pocoo.org/config.html
 
 import os
 import sys
-sys.path.append(os.path.abspath('..'))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'django.conf.global_settings'
+
+sys.path.insert(0, os.path.abspath('..'))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'treebeard.tests.settings'
+
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage',
               'sphinx.ext.graphviz', 'sphinx.ext.inheritance_diagram',
               'sphinx.ext.todo']
@@ -18,7 +20,7 @@ templates_path = ['_templates']
 source_suffix = '.rst'
 master_doc = 'index'
 project = 'django-treebeard'
-copyright = '2008-2010, Gustavo Picon'
+copyright = '2008-2013, Gustavo Picon'
 version = '2.0a1'
 release = '2.0a1'
 exclude_trees = ['_build']
@@ -26,6 +28,9 @@ pygments_style = 'sphinx'
 html_theme = 'default'
 html_static_path = ['_static']
 htmlhelp_basename = 'django-treebearddoc'
-latex_documents = [
-  ('index', 'django-treebeard.tex', 'django-treebeard Documentation',
-   'Gustavo Picon', 'manual')]
+latex_documents = [(
+    'index',
+    'django-treebeard.tex',
+    'django-treebeard Documentation',
+    'Gustavo Picon',
+    'manual')]
