84c8cc0dc5f8a832980c8d7b3a49f9029c8e4cdd
fernando.gutierrez <none@none>
2013-06-16 22:43:05 -0500

    Fix check for is_sorted in treebeard/admin.py, the default value is an empty list and this was checking for None.

--- treebeard/admin.py
+++ treebeard/admin.py
@@ -87,7 +87,7 @@ class TreeAdmin(admin.ModelAdmin):
 
         node = self.get_node(node_id)
         target = self.get_node(target_id)
-        is_sorted = node.node_order_by is not None
+        is_sorted = True if node.node_order_by else False
 
         pos = {
             (True, True): 'sorted-child',
@@ -95,7 +95,6 @@ class TreeAdmin(admin.ModelAdmin):
             (False, True): 'sorted-sibling',
             (False, False): 'left',
         }[as_child, is_sorted]
-
         return self.try_to_move_node(as_child, node, pos, request, target)
 
 
