08dcf2e10e528873f870afffe33e5ee24f3c8510
John Woltman <john@johnwoltman.com>
2013-07-17 16:06:52 -0400

    Added tests package to distribution

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup_args = dict(
     author='Gustavo Picon',
     author_email='tabo@tabo.pe',
     license='Apache License 2.0',
-    packages=['treebeard', 'treebeard.templatetags'],
+    packages=['treebeard', 'treebeard.templatetags','treebeard.tests'],
     package_dir={'treebeard': 'treebeard'},
     package_data={
         'treebeard': ['templates/admin/*.html', 'static/treebeard/*']},
