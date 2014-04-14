0811a97f394de04fe3fc3cfc5ea704ddda8d138e
fernando.gutierrez <none@none>
2014-04-14 12:47:00 -0400

    Added Python 3.4 to setup.py and tox. Tests pass without any changes.

--- setup.py
+++ setup.py
@@ -50,6 +50,7 @@ setup_args = dict(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Operating System :: OS Independent',
         'Topic :: Software Development :: Libraries',
         'Topic :: Utilities'])
