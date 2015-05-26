6e679db354ace8d3ff39a74ae53dfc1e42b2e2e6
Gustavo Picon <tabo@tabo.pe>
2015-05-26 01:10:34 -0500

    Bumping version

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ class pytest_test(test):
 
 setup_args = dict(
     name='django-treebeard',
-    version='3.0',
+    version='3.1a1',
     url='https://tabo.pe/projects/django-treebeard/',
     author='Gustavo Picon',
     author_email='tabo@tabo.pe',
